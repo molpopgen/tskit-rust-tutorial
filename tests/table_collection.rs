edfccce063c67b67f7a99062012cd0291249acf3
Kevin R. Thornton <krthornt@uci.edu>
2022-07-11 12:19:27 -0700

    Add prose/examples to intros and tables. (#2)

--- tests/table_collection.rs
+++ tests/table_collection.rs
@@ -1,19 +1,33 @@
 use tskit_rust_tutorial::tskit;
 
 #[test]
-fn test_simple_table_collection_creation() {
+fn simple_table_collection_creation_with_newtype() {
     #[allow(unused_mut)]
-    // ANCHOR: create_table_collection
-    let mut tables = tskit::TableCollection::new(100.0).unwrap();
-    assert_eq!(tables.sequence_length(), 100.0);
-    // ANCHOR_END: create_table_collection
+    {
+        // ANCHOR: create_table_collection_with_newtype
+        let sequence_length = tskit::Position::from(100.0);
+        let mut tables = tskit::TableCollection::new(sequence_length).unwrap();
+        // In tskit, the various newtypes can be compared to
+        // the low-level types they wrap.
+        assert_eq!(tables.sequence_length(), 100.0);
+        // ANCHOR_END: create_table_collection_with_newtype
+    }
 }
 
 #[test]
-fn test_add_node_without_metadata() {
+fn simple_table_collection_creation() {
+    #[allow(unused_mut)]
+    // ANCHOR: create_table_collection
+    let mut tables = tskit::TableCollection::new(100.0).unwrap();
+    // ANCHOR_END: create_table_collection
+    assert_eq!(tables.sequence_length(), 100.0);
+}
+
+#[test]
+fn add_node_without_metadata() {
     {
-        // ANCHOR: add_node_without_metadata
         let mut tables = tskit::TableCollection::new(100.0).unwrap();
+        // ANCHOR: add_node_without_metadata
         let node_id = tables
             .add_node(
                 0,                         // Node flags
@@ -32,4 +46,23 @@ fn test_add_node_without_metadata() {
         // ANCHOR_END: add_node_without_metadata_using_into
         assert_eq!(node_id, 0);
     }
+}
+
+#[test]
+fn add_node_handle_error() {
+    // ANCHOR: integrity_check
+    let mut tables = tskit::TableCollection::new(100.0).unwrap();
+    // Everything about this edge is wrong...
+    tables.add_edge(-1.0, 11.0, 0, 1).unwrap();
+    // ...and we can catch that here
+    match tables.check_integrity(tskit::TableIntegrityCheckFlags::default()) {
+        Ok(_) => (),
+        // tskit::TskitError can be formatted into the same
+        // error messages that tskit-c/tskit-python give.
+        Err(e) => println!("{}", e),
+    }
+    // ANCHOR_END: integrity_check
+    assert!(tables
+        .check_integrity(tskit::TableIntegrityCheckFlags::default())
+        .is_err());
 }
--- tutorial/src/working_with_table_collections.md
+++ tutorial/src/working_with_table_collections.md
@@ -1,19 +1,50 @@
 # Creating a TableCollection
 
-
 ## Creation
+
+We initialize a `TableCollection` with a sequence length.
+In `tskit-c`, the genome length is a C `double`.
+Here it is a [newtype](https://doc.rust-lang.org/rust-by-example/generics/new_types.html) called `tskit::Position`:
+
+```rust, noplaygound, ignore
+{{#include ../../tests/table_collection.rs:create_table_collection_with_newtype}}
+```
+
+The newtype pattern gives type safety by disallowing you to send a position to a function where a time is required, etc..
+However, it can be inconvenient to type out the full type names every time.
+Thus, the API defines most functions taking arguments `Into<T>` where `T` is one of our newtypes.
+This design means that the following is equivalent to what we wrote above:
 
 ```rust, noplaygound, ignore
 {{#include ../../tests/table_collection.rs:create_table_collection}}
 ```
 
-## Adding rows
+## Adding rows to tables
+
+For each table type (node, edge., etc.), we have a function to add a row.
+For example, to add a node:
 
 ```rust, noplaygound, ignore
 {{#include ../../tests/table_collection.rs:add_node_without_metadata}}
 ```
 
+Again, we can take advantage of being able to pass in any type that is `Into<_>` the required newtype:
+
 ```rust, noplaygound, ignore
 {{#include ../../tests/table_collection.rs:add_node_without_metadata_using_into}}
+```
+
+See the [API docs](https://docs.rs/tskit) for more details and examples.
+
+## Checking table integrity
+
+The data model involves lazy checking of inputs.
+In other words, we can add invalid row data that is not caught by the "add row" functions.
+We inherit this behavior from the C API.
+
+We can check that the tables contain valid data by:
+
+```rust, noplaygound, ignore
+{{#include ../../tests/table_collection.rs:integrity_check}}
 ```
 
